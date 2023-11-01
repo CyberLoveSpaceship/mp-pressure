a3820331998416f545c7676c2907fe6b944ef260
liminalfun <xelanawotka@gmail.com>
2023-11-01 18:38:20 -0400

    [FIX] init issues

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const { i18n } = require("./next-i18next.config")
 
 const nextConfig = {
     env: {
-        // NEXT_PUBLIC_I18N: i18n // alex is having issues here w i18n setup even though it worked last week
+        NEXT_PUBLIC_I18N: i18n // alex is having issues here w i18n setup even though it worked last week
     },
     output: 'export', 
     images: {
