49793c06656b7347a5e8d2abeefdd966bbbf7d08
ox <75142042+oxgr@users.noreply.github.com>
2023-11-02 21:24:52 -0400

    netlify config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  adapter: netlify(),
+  output: "server",
   server: {
     headers: {
       "Access-Control-Allow-Origin": "*",
