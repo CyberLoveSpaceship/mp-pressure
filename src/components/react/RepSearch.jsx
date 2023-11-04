fdf1828b08d91bd762444b2e876e6570c18be3f1
gwuzzy <gwuzzy@users.noreply.github.com>
2023-11-04 06:51:02 +0000

    refactor components to have common react parent

--- src/components/react/RepSearch.jsx
+++ src/components/react/RepSearch.jsx
@@ -1,76 +1,19 @@
-import React from "react";
-import { useStore } from "@nanostores/react";
-import { $searched, $postalCode, $results } from "../../scripts/state";
+// @ts-check
+import RepSearchBox from "./RepSearchBox";
 import RepResults from "./RepResults";
-import "./RepSearch.css";
 
-function RepresentativeSearch() {
-  const [results, setResults] = [useStore($results), $results.set];
-  const [searched, setSearched] = [useStore($searched), $searched.set];
-  const [postalCode, setPostalCode] = [useStore($postalCode), $postalCode.set];
+import styles from "./RepSearch.module.css";
 
-  const clearSearch = () => {
-    setPostalCode("");
-    setResults([]);
-    setSearched(false);
-  };
-
-  const fetchRepresentatives = async () => {
-    console.log("fetch called");
-
-    // manually set postalcode in the end in case of repeat requests
-    const postalCodeElement = document.querySelector("#postalCode");
-    setPostalCode(postalCodeElement.value);
-
-    if (!postalCode) {
-      alert("Please enter a postal code");
-      return;
-    }
-
-    // regex validation
-    const postalCodePattern = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
-
-    // sanitize the postal code
-    const sanitizedPostalCode = postalCode.replace(/\s+/g, "").toUpperCase();
-
-    if (!postalCodePattern.test(sanitizedPostalCode)) {
-      alert("Please enter a valid postal code");
-      return;
-    }
-
-    const response = await fetch(
-      `https://represent.opennorth.ca/postcodes/${encodeURIComponent(
-        sanitizedPostalCode,
-      )}/`,
-    );
-    const data = await response.json();
-
-    if (
-      data.representatives_centroid &&
-      data.representatives_centroid.length > 0
-    ) {
-      setResults(data.representatives_centroid);
-      setSearched(true);
-    } else {
-      setResults([]);
-    }
-  };
-
+export default function RepresentativeSearch() {
   return (
-    <div>
-      <div className="form">
-        <label htmlFor="postalCode">Enter your postal code</label>
-        <input
-          type="text"
-          id="postalCode"
-          value={postalCode}
-          onChange={(e) => setPostalCode(e.target.value)}
-        />
-        <button onClick={fetchRepresentatives}>Next</button>
-        {/* <button onClick={clearSearch}>Clear</button> */}
+    <>
+      <div id="searchView" className={styles["view"]}>
+        <RepSearchBox />
       </div>
-    </div>
+      <div id="resultsView" className={styles["view"]}>
+        <h2 className="resultsHeader">Who to contact</h2>
+        <RepResults />
+      </div>
+    </>
   );
 }
-
-export default RepresentativeSearch;
