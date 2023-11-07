b3d14dac0c46ce527a3422ef02b70334e9690243
gwuzzy <gwuzzy@users.noreply.github.com>
2023-11-07 04:01:25 -0500

    post code as url search param instead of state

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -1,16 +1,17 @@
 "use client";
 
-import { usePathname } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import Link from "next/link";
 
 export default function LocaleSwitcher() {
   const pathName = usePathname();
+  const search = useSearchParams();
   const segments = pathName.split("/");
   const currentLocale = segments[1];
   const otherLocale = currentLocale === "en" ? "fr" : "en";
 
   segments[1] = otherLocale;
-  const uriForOtherLocale = segments.join("/");
+  const uriForOtherLocale = `${segments.join("/")}?${search.toString()}`;
 
   return (
     <div>
