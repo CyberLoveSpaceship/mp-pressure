4192fa349b0cd7cf1e5f5861f4aa67667680a3b5
ox <75142042+oxgr@users.noreply.github.com>
2023-11-09 03:04:27 -0500

    remove tailwind config prose override

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,18 +4,6 @@ import Typography from "@tailwindcss/typography";
 const config: Config = {
   // darkMode: "class",
   content: ["src/**/*.{js,ts,jsx,tsx,mdx}"],
-  theme: {
-    extend: {
-      colors: {},
-      typography: {
-        DEFAULT: {
-          css: {
-            color: "#fff",
-          },
-        },
-      },
-    },
-  },
   plugins: [Typography],
 };
 
