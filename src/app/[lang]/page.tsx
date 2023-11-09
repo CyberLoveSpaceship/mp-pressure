b1a389800a0ed063011f71ea9b8773fb1ed3a9e7
ox <75142042+oxgr@users.noreply.github.com>
2023-11-09 06:03:09 -0500

    parse heading from frontmatter + parse locales based on folder

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -3,12 +3,16 @@ import RepSearch from "~/components/RepSearch";
 import path from "path";
 import fs from "fs/promises";
 import Markdown from "react-markdown";
+import matter from "gray-matter";
 import { ReactElement } from "react";
 
 export type DropdownItem = {
   heading: string;
   content: ReactElement<any, any>;
 };
+
+type Langs = "en" | "fr";
+type LangDropdowns = { [L in Langs]: DropdownItem[] };
 
 export default async function Home() {
   const allDropdownItems = await getDropdownItems();
@@ -20,31 +24,50 @@ export default async function Home() {
   );
 }
 
-export async function getDropdownItems() {
-  const dropdownsContentPath = path.join("public", "content");
+async function getDropdownItems() {
+  const dropdownsContentPath = path.join("public", "content", "dropdowns");
+  const langs = await fs.readdir(dropdownsContentPath);
 
-  const fileNames = await fs.readdir(dropdownsContentPath);
+  // get all dropdowns into array of obj
+  const dropdowns: LangDropdowns = (
+    await Promise.all(
+      langs.map(async (lang) => {
+        const langPath = path.join(dropdownsContentPath, lang);
+        const fileNames = await fs.readdir(langPath);
 
-  // process files into Dropdown objects
-  const dropdowns: DropdownItem[] = await Promise.all(
-    fileNames.map(async (fileName, index) => {
-      const filePath = path.join(dropdownsContentPath, fileName);
-      const heading = path.parse(fileName).name.replaceAll(/^\d-/g, "");
-      const contentRaw = await fs.readFile(filePath, { encoding: "utf8" });
-      const contentRendered = (
-        <>
-          <Markdown>{contentRaw}</Markdown>
-        </>
-      );
-      const dropdownContent: DropdownItem = {
-        heading,
-        content: contentRendered,
-      };
-      return dropdownContent;
-    }),
-  );
+        // get dropdowns per lang
+        const langDropdowns = await Promise.all(
+          fileNames.map(async (fileName) => {
+            const filePath = path.join(langPath, fileName);
+            const mdRaw = await fs.readFile(filePath, { encoding: "utf8" });
+            const md = matter(mdRaw);
+            const mdRendered = (
+              <>
+                <Markdown>{md.content}</Markdown>
+              </>
+            );
+            const dropdownContent: DropdownItem = {
+              heading: md.data?.heading ?? "",
+              content: mdRendered,
+            };
+            return dropdownContent;
+          }),
+        );
 
-  return {
-    en: dropdowns,
-  };
+        return {
+          key: lang,
+          val: langDropdowns,
+        } as {
+          key: Langs;
+          val: DropdownItem[];
+        };
+      }),
+    )
+  ).reduce((acc, curr) => {
+    // merge objects into single object with lang keys
+    acc[curr.key] = curr.val;
+    return acc;
+  }, {} as LangDropdowns);
+
+  return dropdowns;
 }
